0cc20480eca610c7abfa0137bd502d84ee0e2aa7
Davi Bortolini <davibortolini@hotmai.com>
2022-05-28 03:04:06 -0300

    update

--- ex23_usando_calculo.py
+++ ex23_usando_calculo.py
@@ -1,7 +1,6 @@
 from sqlalchemy import true
 
-entrar = 's' #laço tratamento de erro /excedeu o tamanho da lista
-while entrar == 's':
+while true: # repete todo o script (erro de numero acima de 9999)
 
     while true: # laço para tratamento de erro para aceitar somente números
         try: # enquanto a atribuição a variavel número não for inteiro 
@@ -10,47 +9,24 @@ while entrar == 's':
         except ValueError: # trata o erro antes de reiniciar
             print('Ops! Você deve digitar um número!')
 
-    #lista_numero = str(numero) # converte em string e divide os elementos em lista.
-
-    """if len(lista_numero) >= 5:  # verifica o tamanho da string e retorna erro se for maior ou igua a 5  
+    if numero >= 9999:  # verifica o tamanho da string e retorna erro se for maior ou igua a 5  
         x = str(input('Ops! Digite um número entre 0 e 9999 \nDeseja reiniciar o programa? Se SIM digite [s] ou qualquer tecla para encerrar!')) 
         if x == 's': 
             print('recomeçando!')
+            
         else:
             print('Programa encerrado!')
-            exit(0)
-        
-   # verifica o tamanho da lista e atribui os valores
-    elif len(lista_numero) == 4:
-        print(f'A unidade é {lista_numero[3]}') 
-        print(f'A Dezena é {lista_numero[2]}')
-        print(f'A centena é {lista_numero[1]}')
-        print(f'O milhar é {lista_numero[0]}')
+            break
+
+        #RESOLUÇAO USANDO MATEMÁTICA
+    else:    
+        unidade = numero // 1 % 10
+        dezena = numero // 10 % 10
+        centena = numero // 100 % 10
+        milhar = numero // 1000 % 10
+
+        print(f'A unidade é {unidade}') 
+        print(f'A Dezena é {dezena}')
+        print(f'A centena é {centena}')
+        print(f'O milhar é {milhar}')
         exit(0)
-        
-
-    elif len(lista_numero) == 3:
-        print(f'A unidade é {lista_numero[2]}') 
-        print(f'A Dezena é {lista_numero[1]}')
-        print(f'A centena é {lista_numero[0]}')
-        exit(0)
-
-    elif len(lista_numero) == 2:
-        print(f'A Dezena é {lista_numero[1]}')
-        print(f'A centena é {lista_numero[0]}')
-        exit(0)
-    else:
-        print(f'A Dezena é {lista_numero[0]}')
-        exit(0)"""
-
-#RESOLUÇAO USANDO MATEMÁTICA
-    unidade = numero // 1 % 10
-    dezena = numero // 10 % 10
-    centena = numero // 100 % 10
-    milhar = numero // 1000 % 10
-
-    print(f'A unidade é {unidade}') 
-    print(f'A Dezena é {dezena}')
-    print(f'A centena é {centena}')
-    print(f'O milhar é {milhar}')
-    exit(0)
