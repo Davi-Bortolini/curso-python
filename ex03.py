ef4df1c9817fc1ba30f31f4a664e061eaea265c2
Davi-Bortolini <davibortolini@hotmail.com>
2023-05-05 02:22:02 -0300

    recomeçando

---
+++ ex03.py
@@ -0,0 +1,7 @@
+"""Crie um programa que leia dois números e mostre a soma entre eles"""
+
+x1 = float(input("Digite um número: "))
+x2 = float(input("Digite outro número: "))
+y = x1 + x2
+print(f"A soma entre os dois números é {x1} e {x2} é: {y}")
+
