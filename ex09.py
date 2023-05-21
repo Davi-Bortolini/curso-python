23b61e17ad68140519d951bbd8100a50d66ed1f8
Davi-Bortolini <davibortolini@hotmail.com>
2023-05-21 05:03:14 -0300

    20/05

---
+++ ex09.py
@@ -0,0 +1,7 @@
+print('Escreva um programa que leia um valor em metros, e exiba ele em centímetros e milímetros!')
+
+x = float(input('Digite um valor em metros: '))
+
+print(f'O valor {x} metros convertidos em milímetros é: {x * 1000:.0f} mm!')
+print(f'O valor {x} metros convertidos em centímetros é {x * 100:.0f} cm!')
+
